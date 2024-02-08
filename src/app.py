365ae153b51b4608a174b7e59a9f470c313c832d
GadeaP <148185671+GadeaP@users.noreply.github.com>
2024-02-08 15:20:05 +0000

    terminado

--- src/app.py
+++ src/app.py
@@ -1,4 +1,49 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+train_data = pd.read_csv("../data/processed/clean_train.csv")
+test_data = pd.read_csv("../data/processed/clean_test.csv")
 
-# your code here
+train_data.head()
+X_train = train_data.drop(["y_n"], axis = 1)
+y_train = train_data["y_n"]
+X_test = test_data.drop(["y_n"], axis = 1)
+y_test = test_data["y_n"]
+
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+y_pred
+
+from sklearn.metrics import accuracy_score
+accuracy_score(y_test, y_pred)
+
+
+
+#optimizacion
+from sklearn.model_selection import GridSearchCV
+hyperparams = {
+    "C": [0.001, 0.01, 0.1, 1, 10, 100, 1000],
+    "penalty": ["l1", "l2", "elasticnet", None],
+    "solver": ["newton-cg", "lbfgs", "liblinear", "sag", "saga"]
+}
+grid = GridSearchCV(model, hyperparams, scoring = "accuracy", cv = 10)
+grid
+
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+
+grid.fit(X_train, y_train)
+
+print(f"Best hyperparameters: {grid.best_params_}")
+model = LogisticRegression(C = 0.1, penalty = "l2", solver = "liblinear")
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+y_pred
+
+accuracy_score(y_test, y_pred)
+
+from pickle import dump
+dump(model, open("../models/logistic_regression_C-0.1_penalty-l2_solver-liblinear_42.sav", "wb"))
